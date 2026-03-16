5eab9dd9c0a7113f453ff18fd0922cc9947533e7
Montasser Chebbah <montaonline17@gmail.com>
2026-03-16 14:39:51 +0100

    Everything dark

--- components/ui/preloader-heatmap.jsx
+++ components/ui/preloader-heatmap.jsx
@@ -35,6 +35,7 @@ export function PreloaderHeatmap() {
             width: "100%",
             height: "100%",
             display: "block",
+            background: "#000000",
           }}
         />
       </div>
